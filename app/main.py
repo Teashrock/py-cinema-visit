acefb3e83068c93690e2842ccfb1ba144ec62a8c
Teashrock <kajitsu22@gmail.com>
2026-06-10 23:31:19 +0300

    Flake8

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
     hall = CinemaHall(hall_number)
     cleaning_staff = Cleaner(cleaner)
     customer_list = []
